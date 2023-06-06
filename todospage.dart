09670a8f38eda5643b3288d513e501b353446a2a
Cenvincent <129710260+Cenvincent@users.noreply.github.com>
2023-06-06 11:26:28 +0700

    Update todospage.dart

--- todospage.dart
+++ todospage.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:date_time_picker/date_time_picker.dart';
 import 'package:intl/intl.dart';
-
 class Todos extends StatefulWidget {
   const Todos({Key? key, required this.onSaveTodo}) : super(key: key);
-
   final Function onSaveTodo;
-
   @override
   _TodosState createState() => _TodosState();
 }
-
 class _TodosState extends State<Todos> {
-  String dropdownvalue = 'Work';
+  DateTime _date = DateTime.now();
+  String dropdownvalue = 'Routine';
   final List<String> items = ['Routine', 'Work', 'Others'];
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
   final TextEditingController _startDateController = TextEditingController();
   final TextEditingController _endDateController = TextEditingController();
-
   @override
   void dispose() {
     _titleController.dispose();
@@ -27,14 +22,13 @@ class _TodosState extends State<Todos> {
     _endDateController.dispose();
     super.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
         appBar: AppBar(
           title: const Text('Todos'),
           centerTitle: true,
+          backgroundColor: Colors.blue,
         ),
         body: Column(
           children: [
@@ -97,33 +91,64 @@ class _TodosState extends State<Todos> {
               children: <Widget>[
                 const Padding(padding: EdgeInsets.all(20.0)),
                 Expanded(
-                  child: DateTimePicker(
-                        initialValue: '',
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2100),
-                        dateLabelText: 'Date',
-                        onChanged: (val) => print(val),
-                        validator: (val) {
-                          print(val);
-                          return null;
-                        },
-                        onSaved: (val) => print(val),
-                      )
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      controller: _startDateController,
+                      enabled: true,
+                      readOnly: true,
+                      decoration: const InputDecoration(
+                        alignLabelWithHint: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        ),
+                      ),
+                      onTap: () async {
+                        var res = await showDatePicker(
+                            context: context,
+                            initialDate: _date,
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2500));
+                        if (res != null) {
+                          setState(() {
+                            _date = res;
+                            String _formatted = DateFormat('d MMM yyyy').format(_date);
+                            _startDateController.text = _formatted;
+                          });
+                        }
+                      },
+                    ),
+                  ),
                 ),
-                SizedBox(width: 50,),
                 Expanded(
-                  child: DateTimePicker(
-                        initialValue: '',
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2100),
-                        dateLabelText: 'Date',
-                        onChanged: (val) => print(val),
-                        validator: (val) {
-                          print(val);
-                          return null;
-                        },
-                        onSaved: (val) => print(val),
-                      )
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      controller: _endDateController,
+                      enabled: true,
+                      readOnly: true,
+                      decoration: const InputDecoration(
+                        alignLabelWithHint: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        ),
+                      ),
+                      onTap: () async {
+                        var res = await showDatePicker(
+                            context: context,
+                            initialDate: _date,
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2500));
+                        if (res != null) {
+                          setState(() {
+                            _date = res;
+                            String _formatted = DateFormat('d MMM yyyy').format(_date);
+                            _endDateController.text = _formatted;
+                          });
+                        }
+                      },
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -163,7 +188,7 @@ class _TodosState extends State<Todos> {
                         padding: const EdgeInsets.all(10.0),
                         child: OutlinedButton(
                           style: OutlinedButton.styleFrom(
-                            foregroundColor: Colors.blue,
+                            foregroundColor: Colors.purple,
                           ),
                           onPressed: () {
                             Navigator.pop(context);
@@ -185,63 +210,72 @@ class _TodosState extends State<Todos> {
                               _endDateController.text,
                               dropdownvalue);
                           Navigator.pop(context, 'Data saved successfully.');
-                            showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Dialog(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5)),
-                        child: Stack(
-                          alignment: Alignment.center,
-                          clipBehavior: Clip.none,
-                          children: [
-                            Container(
-                              height: 180,
-                              width: 250,
-                              child: Padding(
-                                padding: EdgeInsets.fromLTRB(5, 50, 5, 5),
-                                child: Column(
-                                  children: [
-                                    Text("Berhasil",
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                        textAlign: TextAlign.center),
-                                    SizedBox(height: 10),
-                                    Text("Kegiatan berhasil ditambahkan",
-                                        style: TextStyle(fontSize: 14),
-                                        textAlign: TextAlign.center),
-                                    SizedBox(height: 20),
-                                    ElevatedButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text("OK"),
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor: Colors.green,
-                                          padding: EdgeInsets.symmetric(
-                                              vertical: 15, horizontal: 35)),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                                top: -30,
-                                child: CircleAvatar(
-                                  radius: 30,
-                                  backgroundColor: Colors.white,
-                                  child: CircleAvatar(
-                                    backgroundColor: Colors.green,
-                                    radius: 26,
-                                    child: Icon(Icons.check,
-                                        size: 30, color: Colors.white),
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5)),
+                                  child: Stack(
+                                    alignment: Alignment.center,
+                                    clipBehavior: Clip.none,
+                                    children: [
+                                      Container(
+                                        height: 180,
+                                        width: 250,
+                                        child: Padding(
+                                          padding:
+                                              EdgeInsets.fromLTRB(5, 50, 5, 5),
+                                          child: Column(
+                                            children: [
+                                              Text(
+                                                "Berhasil",
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              SizedBox(height: 10),
+                                              Text(
+                                                "Kegiatan berhasil ditambahkan",
+                                                style: TextStyle(fontSize: 16),
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              SizedBox(height: 20),
+                                              ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: Text("OK"),
+                                                style: ElevatedButton.styleFrom(
+                                                    primary: Colors.green,
+                                                    padding:
+                                                        EdgeInsets.symmetric(
+                                                            vertical: 15,
+                                                            horizontal: 35)),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: -30,
+                                        child: CircleAvatar(
+                                          radius: 30,
+                                          backgroundColor: Colors.white,
+                                          child: CircleAvatar(
+                                            backgroundColor: Colors.green,
+                                            radius: 25,
+                                            child: Icon(Icons.check,
+                                                size: 30, color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                )),
-                          ],
-                        ),
-                      );
-                    });
+                                );
+                              });
                         },
                         child: const Text('Simpan'),
                       )),
